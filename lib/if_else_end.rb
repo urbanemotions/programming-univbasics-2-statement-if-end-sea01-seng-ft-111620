441a673bbc5a130f84123b3a6c3c8a545660b85e
Sin Wildy <sw.wildy@gmail.com>
2020-10-11 22:12:34 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -4,19 +4,11 @@ current_time = Time.now
 current_time = current_time.to_i 
 
 if current_time % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
   
-
-current_time.to_s = (2 || 4 || 6 || 8 || 0) 
-if current
-#even = 2 || 4 || 6 || 8 || 0
-#if current_time.to_s = even  
-#  puts "Even"
-#else
-#  puts "Odd"
-#end 
-
-
-
 
 
 #this_year = Time.now.year
