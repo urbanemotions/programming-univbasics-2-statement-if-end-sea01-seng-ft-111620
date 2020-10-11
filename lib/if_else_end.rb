c9a780b3979be1675541c041f6aa6ec63b84b991
Sin Wildy <sw.wildy@gmail.com>
2020-10-11 22:09:55 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,11 @@
 # Write your solution here
 
 current_time = Time.now
+current_time = current_time.to_i 
+
+if current_time % 2 == 0
+  
+
 current_time.to_s = (2 || 4 || 6 || 8 || 0) 
 if current
 #even = 2 || 4 || 6 || 8 || 0
