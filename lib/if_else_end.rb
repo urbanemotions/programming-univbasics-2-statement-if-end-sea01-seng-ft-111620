37716825462935a76d836706ccb525967d0892d2
Sin Wildy <sw.wildy@gmail.com>
2020-10-11 22:30:53 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,100 @@
 # Write your solution here
+
+current_time = Time.now
+current_time = current_time.to_i
+
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
+
+#if Time.now.to_i % 2 == 0
+#  puts "Even!"
+#else
+#  puts "Odd!"
+#end
+  
+#current_time = Time.now
+#current_time = current_time.to_i 
+#
+#if current_time % 2 == 0
+#  puts "Even!"
+#else
+#  puts "Odd!"
+#end
+  
+
+
+#this_year = Time.now.year
+#puts "Hey, it's not 2019!" unless this_year == 2019
+
+
+
+#puts "You know what year it is??"
+#this_year = 2019
+#puts "Hey, it's 2019!" if this_year == 2019
+
+
+
+#chance_of_rain = -23
+#
+#if chance_of_rain <= 0.25 
+#  puts "Pack a sun shelter!"
+#elsif (chance_of_rain > 0.25 && chance_of_rain <0.75)
+#  puts "Pack an umbrella!"
+#else
+#  puts "Stay home and read Hegel."
+#end 
+
+
+
+#chance_of_rain = 1
+#
+#puts "Let's go outside!"
+#
+#if chance_of_rain > 0.5   
+#  puts "Pack an umbrella!"
+#else
+#  puts "Enjoy the fine day!"
+#end
+#
+#puts "Oh, and always wear sunscreen!"
+
+
+
+#chance_of_rain = 0.2
+#
+#puts "Let's go outside!"
+#
+#if chance_of_rain > 0.5   
+#  puts "Pack an umbrella!"
+#else
+#  puts "Enjoy the fine day!"
+#end
+#
+#puts "Oh, and always wear sunscreen!"
+
+
+
+#run_code_inside = FALSE
+#
+#puts "Code before if ... end" 
+#
+#if run_code_inside 
+#  puts "code inside"
+#end
+#
+#puts "code after if ... end"
+
+
+
+#run_code_inside = TRUE
+#
+#puts "Code before if ... end" 
+#
+#if run_code_inside 
+#  puts "code inside"
+#end
+#
+#puts "code after if ... end"
