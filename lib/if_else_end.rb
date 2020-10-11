ca7438ced7ba627f93561286425f426d0877c190
Sin Wildy <sw.wildy@gmail.com>
2020-10-11 21:25:21 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,75 @@
 # Write your solution here
+
+
+#this_year = Time.now.year
+#puts "Hey, it's not 2019!" unless this_year == 2019
+
+
+
+#puts "You know what year it is??"
+#this_year = 2019
+#puts "Hey, it's 2019!" if this_year == 2019
+
+
+
+#chance_of_rain = -23
+#
+#if chance_of_rain <= 0.25 
+#  puts "Pack a sun shelter!"
+#elsif (chance_of_rain > 0.25 && chance_of_rain <0.75)
+#  puts "Pack an umbrella!"
+#else
+#  puts "Stay home and read Hegel."
+#end 
+
+
+
+#chance_of_rain = 1
+#
+#puts "Let's go outside!"
+#
+#if chance_of_rain > 0.5   
+#  puts "Pack an umbrella!"
+#else
+#  puts "Enjoy the fine day!"
+#end
+#
+#puts "Oh, and always wear sunscreen!"
+
+
+
+#chance_of_rain = 0.2
+#
+#puts "Let's go outside!"
+#
+#if chance_of_rain > 0.5   
+#  puts "Pack an umbrella!"
+#else
+#  puts "Enjoy the fine day!"
+#end
+#
+#puts "Oh, and always wear sunscreen!"
+
+
+
+#run_code_inside = FALSE
+#
+#puts "Code before if ... end" 
+#
+#if run_code_inside 
+#  puts "code inside"
+#end
+#
+#puts "code after if ... end"
+
+
+
+#run_code_inside = TRUE
+#
+#puts "Code before if ... end" 
+#
+#if run_code_inside 
+#  puts "code inside"
+#end
+#
+#puts "code after if ... end"
