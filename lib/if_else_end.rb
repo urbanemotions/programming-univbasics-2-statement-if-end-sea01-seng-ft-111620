d76a4f1653b557ca793ce0ee55c2b5e37b5fffe3
Sin Wildy <sw.wildy@gmail.com>
2020-10-11 22:21:38 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,15 +1,19 @@
 # Write your solution here
 
-current_time = Time.now
-current_time = current_time.to_i 
-
-puts Time.now
-puts current_time.to_i
-if current_time % 2 == 0
+if Time.now.to_i % 2 == 0
   puts "Even!"
 else
   puts "Odd!"
 end
+  
+#current_time = Time.now
+#current_time = current_time.to_i 
+#
+#if current_time % 2 == 0
+#  puts "Even!"
+#else
+#  puts "Odd!"
+#end
   
 
 
